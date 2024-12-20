af55f3ccae858272caa1962c8019f6f4f9ad8a74
Nate Potter <nathanielpotter@gmail.com>
2024-12-20 19:30:55 +0100

    Initial commit

---
+++ Sources/DependencyBImplementation/DependencyBImplementation.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+public struct DependencyB {
+    public init() {
+        
+    }
+    
+    public func doSomething() -> String {
+        "Directly Imported Dependency"
+    }
+}
